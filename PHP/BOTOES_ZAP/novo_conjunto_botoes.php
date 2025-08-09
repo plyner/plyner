208380c08cf3b7f0fbc0808563ab02e5384d3a96
Plyner Networker <166955789+plyner@users.noreply.github.com>
2025-08-09 10:38:30 -0300

    Add files via upload

---
+++ PHP/BOTOES_ZAP/novo_conjunto_botoes.php
@@ -0,0 +1,50 @@
+<?php
+
+//**************** Dados Basicos para Adicionar um Novo Enviador WhatsApp **************************
+		//1 - Crie uma conta no Plyner, para definir seus dados de acesso: meuid e senha
+		//2 - Apos criar sua conta no Plyner, defina um numero enviador para realizar seus testes de envio de mensagem pelo WhatsApp
+		//ATENÇAO: Caso tenha duvidas em criar sua conta no Plyner, acesse a academia Plyner. https://plyner.com.br/academia
+		//*** Para realizar este teste, serão necessários os dados: meuid(seu id mostrado nos dados de acesso), senha(sua senha para acessar o Plyner)
+
+		$curl = curl_init();
+
+		// Define a URL da API
+		$url = 'https://api.plyner.com.br/zap';
+
+		$acaobotao = 'criarconjunto'; // criarconjunto, removerconjunto, adicionaritem, conjuntosativos
+        $tipo = 2; // 1 = lista de opcoes, 2 = conjunto de botoes
+        $apelido = 'Conjunto teste 1'; // nome deste conjunto de botoes dentro do seu Plyner (Opcional)
+        $titulo = 'Como voce deseja ser atendido...'; // Titulo que sera mostrado na mensagem do Whastapp
+        $exclusao = 1; // 1 = Sera auto excluido em 30 dias, 0 = Nao sera auto excluido
+
+		// Define o corpo da requisição como um JSON
+		$data = array('meuid' => 36, 'senha' => '2222', 'acaobotao' => $acaobotao, 'tipo' => $tipo, 'apelido' => $apelido, 'titulo' => $titulo, 'exclusao' => $exclusao );
+		$data_string = json_encode($data);
+
+		// Define as opções da requisição
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+		    'Content-Type: application/json',
+		    'Content-Length: ' . strlen($data_string)
+		));
+
+		// Envia a requisição e obtém a resposta
+		$response = curl_exec($curl);
+
+		// Fecha a sessão cURL
+		curl_close($curl);
+
+		//Corrige barras invertidas caso existam
+		$response = stripslashes($response);
+
+		//Grava resposta em um txt
+		$arquivo ='retorno.txt'; 
+		$sucesso = file_put_contents( $arquivo , $response);
+
+		// Exibe a resposta
+		echo $response;
+
+?>
